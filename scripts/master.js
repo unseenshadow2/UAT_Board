bbfe36a16486ef29df661f925192ca4ccaf982a3
Anthony Parsch <anthonyparsch@gmail.com>
2017-10-23 15:12:21 -0700

    Added active button in nav bar

--- scripts/master.js
+++ scripts/master.js
@@ -1,3 +1,7 @@
+var homepage;
+var gamepage;
+var acspage;
+
 function LoadPage(url, id)
 {
 	// Set default
@@ -6,4 +10,33 @@ function LoadPage(url, id)
 	var page_area = document.getElementById(id);
 	
 	page_area.innerHTML = read_contents(url);
+}
+
+function SetActive(button_id)
+{
+	// Cleanse our active button
+	ResetButtonActive(homepage);
+	ResetButtonActive(gamepage);
+	ResetButtonActive(acspage);
+	
+	// Set the button to active
+	var button = document.getElementById(button_id);
+	button.classList.add('active');
+	if (button.classList.contains('navigator')) button.classList.remove('navigator');
+}
+
+function ResetButtonActive(button)
+{
+	if (button.classList.contains('active')) 
+	{
+		button.classList.remove('active');
+		button.classList.add('navigator');
+	}
+}
+
+function SetupButtons()
+{
+	homepage = document.getElementById("home_button");
+	gamepage = document.getElementById("game_button");
+	acspage = document.getElementById("acs_button");
 }
